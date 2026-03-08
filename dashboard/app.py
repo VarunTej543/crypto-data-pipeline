12c86ae1e4853c4de105fa99d7ed8479cd749a59
VarunTej543 <varuntejaruntej007@gmail.com>
2026-03-08 10:56:08 +0530

    Day 6:feat: add crypto ETL pipeline with data extraction, transformation, loading into PostgreSQL and visualization using Streamlit

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,12 @@
+from streamlit_autorefresh import st_autorefresh
+
 import streamlit as st
 import pandas as pd
 from database.db_connection import get_connection
 
 st.title("📊 Crypto Price Dashboard")
+
+st_autorefresh(interval=10000, key="crypto_refresh")
 
 conn = get_connection()
 
@@ -23,3 +27,22 @@ st.subheader("Price Chart")
 chart_data = df.sort_values("timestamp")
 
 st.line_chart(chart_data.set_index("timestamp")["price"])
+
+st.subheader("Top 10 Cryptocurrencies by Market Cap")
+
+top_query = """
+SELECT coin_name, price, market_cap
+FROM crypto_prices
+ORDER BY market_cap DESC
+LIMIT 10
+"""
+
+top_df = pd.read_sql(top_query, conn)
+
+st.dataframe(top_df)
+
+st.subheader("Market Cap Comparison")
+
+chart_df = top_df.set_index("coin_name")
+
+st.bar_chart(chart_df["market_cap"])
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+import time
+
 from etl.extract import extract
 from etl.transform import transform
 from etl.load import load
 from database.init_db import initialize_database
 
+
 def run_pipeline():
-
-    initialize_database()
-
     raw_data = extract()
-
     transformed_data = transform(raw_data)
-
     load(transformed_data)
 
     print("ETL Pipeline completed successfully!")
 
+
 if __name__ == "__main__":
-    run_pipeline()
+
+    # create database tables once
+    initialize_database()
+
+    while True:
+        try:
+            run_pipeline()
+        except Exception as e:
+            print("Error:", e)
+
+        print("Waiting 60 seconds for next run...\n")
+        time.sleep(60)
